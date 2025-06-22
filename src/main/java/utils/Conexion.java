97bc56c202f01fd7211bfb6a3639148d51ac6034
Alejo Legname <legname.alejo@gmail.com>
2025-06-22 20:24:02 -0300

    Fix Conexion db

--- src/main/java/utils/Conexion.java
+++ src/main/java/utils/Conexion.java
@@ -4,54 +4,39 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conexion 
-{
-	public static Conexion instancia;
-	private Connection connection;
-	
-	private Conexion()
-	{
-		try
-		{	
-			// Para MySQL Connector 8.0+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/[DB_NAME]";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+    private static Conexion instancia;
+    
+    private Conexion() {
+        try {
+        	// Para MySQL Connector 8.0+
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
 			// Para MySQL Connector 5.1.x
 			// Class.forName("com.mysql.jdbc.Driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/[DBNAME]","root","root");
-			this.connection.setAutoCommit(false);
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public static Conexion getConexion()   
-	{								
-		if(instancia == null)
-		{
-			instancia = new Conexion();
-		}
-		return instancia;
-	}
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Connection getSQLConexion() 
-	{
-		return this.connection;
-	}
-	
-	public void cerrarConexion()
-	{
-		try 
-		{
-			this.connection.close();
-		}
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
-		instancia = null;
-	}
+    public static Conexion getConexion() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+
+    public Connection getSQLConexion() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn.setAutoCommit(false);
+            return conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
