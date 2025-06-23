55c4a457ca72ea6485bfd3faf9a33f5db08efcad
Alejo Legname <legname.alejo@gmail.com>
2025-06-22 21:54:12 -0300

    Login real

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -7,50 +7,54 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
+import dominio.Usuario;
+import excepciones.AutenticacionException;
+import negocioImpl.AutenticacionNegocioImpl;
 import java.io.IOException;
-
 
 @WebServlet("/ServletLogin")
 public class ServletLogin extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
 
     public ServletLogin() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nombreUsuario = request.getParameter("usuario");
+        String clave = request.getParameter("clave");
 
-	
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 String usuario = request.getParameter("usuario");
-	        String clave = request.getParameter("clave");
+        AutenticacionNegocioImpl authNegocio = new AutenticacionNegocioImpl();
 
-	        // Autenticación de prueba
-	        if ("admin".equals(usuario) && "1234".equals(clave)) {
-	            HttpSession session = request.getSession();
-	            session.setAttribute("usuario", usuario);
-	            session.setAttribute("rol", "admin");
-	            
-	            response.sendRedirect(request.getContextPath() + "/ServletMenuAdmin");
-	        } 
-	        else if ("cliente".equals(usuario) && "4321".equals(clave)) {
-	        	HttpSession session = request.getSession();
-	            session.setAttribute("usuario", usuario);
-	            session.setAttribute("rol", "admin");
-	            
-	            response.sendRedirect(request.getContextPath() + "/ServletMenuCliente");
-	        }
-	        else {
-	            request.setAttribute("errorLogin", "Usuario o contraseña incorrectos.");
-	            request.getRequestDispatcher("/jsp/comunes/login.jsp").forward(request, response);
-	        }
+        try {
+            Usuario usuario = authNegocio.autenticar(nombreUsuario, clave);
+
+            HttpSession session = request.getSession();
+            session.setAttribute("usuario", usuario);
+
+            if (usuario.getTipo().equalsIgnoreCase("bancario")) {
+                response.sendRedirect(request.getContextPath() + "/ServletMenuAdmin");
+
+                if (usuario.isAdmin()) {
+                    session.setAttribute("rol", "admin");
+                } else {
+                    session.setAttribute("rol", "bancario");
+                }
+            } else {
+                // ACÁ HAY QUE VALIDAR QUE EL USUARIO ESTÉ LINKEADO A UNA INSTANCIA DE CLIENTES
+                session.setAttribute("rol", "cliente");
+                response.sendRedirect(request.getContextPath() + "/ServletMenuCliente");
+            }
+
+        } catch (AutenticacionException e) {
+            request.setAttribute("errorLogin", e.getMessage());
+            request.getRequestDispatcher("/jsp/comunes/login.jsp").forward(request, response);
+        }
     }
 
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/jsp/comunes/login.jsp");
+    }
 }
