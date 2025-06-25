f8cdd06b88e7ed47f6dab6f75bfd13dfb17233de
Alejo Legname <legname.alejo@gmail.com>
2025-06-25 17:55:59 -0300

    Validar rol user

--- src/main/java/negocio/AutenticacionNegocio.java
+++ src/main/java/negocio/AutenticacionNegocio.java
@@ -5,4 +5,5 @@ import excepciones.AutenticacionException;
 
 public interface AutenticacionNegocio {
     Usuario autenticar(String nombreUsuario, String clave) throws AutenticacionException;
+    boolean validarRolAdmin(Usuario usuario);
 }
